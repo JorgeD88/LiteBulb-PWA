7f6d4ed115d31b5de06fda52555cf9deb9ad9c80
Jorge De La Cruz <delacruzruben88@gmail.com>
2025-11-02 08:37:12 -0600

    feat: Add PWA support with service worker and manifest

---
+++ js/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'litebulb-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/manifest.json',
+  '/images/Lightbulb.jpg',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
